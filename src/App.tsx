108efa154460351373a1c58dbb32a6ea73f4036b
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-09-07 19:26:07 -0500

    First Job item in Root App

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,13 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Johan Vargas</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h2 className="job-title">Sr. Frontend Developer</h2>
+        <p className="job-description">Features and bugfix in pags build with AEM, fully integrated with Adobe Experience Manager and using some features likes SASS, LESS, Vanilla JS, React and all the frontend part together AEM support</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
