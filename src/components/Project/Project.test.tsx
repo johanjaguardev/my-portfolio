f72567a09e74b44c472325fb114ed05917699f42
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2023-01-18 20:39:15 -0500

    fixing mock in project test to fix deploy

--- src/components/Project/Project.test.tsx
+++ src/components/Project/Project.test.tsx
@@ -17,12 +17,24 @@ describe("Project Component", (): void => {
       stack: ["React", "Node"],
       thumbnail: ["www.test.jpg"],
       videoUrl: "www.test.mp4",
-      charge: "Test Charge",
-      challenge: "Test Challenge",
-      solution: "Test Solution",
-      result: "Test Result",
-      achievement: [{ number: 1, description: "Test Achievement" }],
-      observations: "Test Observations",
+      caseOfUse: {
+        charge:
+          "Create a library for a series of specialized documents on classical studies for the National Online University, organized and available to each of the students through its website, in a manageable and responsive way.",
+        challenge:
+          "Organize the existing .pdf files and easily index them in a user-friendly development for making them editable and with the possibility of adding new books, and to view this catalog of books in a progressive web application in a simple and orderly way.",
+        solution:
+          "I created a script to index existing documents and generated a custom post type for WordPress with it, and through this allowed to add books through the WordPress site, while in another directory an Angular application was created to view the generated library and generate its search within the catalog.",
+        result:
+          "A digital book manager was achieved in the WordPress interface, which is one of the most common for blogging and website creation, uploading the different books from there onwards, and updated in real time to be consulted by the client in Angular, both from mobile and from a PC through the website http://xenia.bogota.unal.edu.co/.",
+      },
+      achievement: [
+        {
+          number: 1,
+          description: "Most used search engine in the world",
+        },
+      ],
+      observations:
+        "Google Search is one of the most used search engines in the world",
     };
   });
   afterEach(cleanup);
@@ -53,19 +65,19 @@ describe("Project Component", (): void => {
   });
 
   test("should have a charge property as string and optional", () => {
-    expect(typeof project.charge).toBe("string");
+    expect(typeof project.caseOfUse?.charge).toBe("string");
   });
 
   test("should have a challenge property as string and optional", () => {
-    expect(typeof project.challenge).toBe("string");
+    expect(typeof project.caseOfUse?.challenge).toBe("string");
   });
 
   test("should have a solution property as string and optional", () => {
-    expect(typeof project.solution).toBe("string");
+    expect(typeof project.caseOfUse?.solution).toBe("string");
   });
 
   test("should have a result property as string and optional", () => {
-    expect(typeof project.result).toBe("string");
+    expect(typeof project.caseOfUse?.result).toBe("string");
   });
 
   // test("should have a achievement property as an array of objects optional each one with number and description as properties", () => {
